77d07716dd1362f1695dfc60cae55b8864284fa0
Amanda Silva <amanda.sp241@gmail.com>
2023-09-04 15:56:18 -0300

    rename image

--- page-cmt/script.js
+++ page-cmt/script.js
@@ -3,19 +3,19 @@ var app = angular.module('myApp', []);
 app.controller('cardComentario', function($scope) {
     $scope.cardComentario = [
         {
-            userImage: '/images/Ellipse 1.png',
+            userImage: '../images/Ellipse 1.png',
             userComentario: 'Minha viagem foi simplesmente incrível! Explorar novos lugares e culturas é uma experiência que todos deveriam vivenciar. Mal posso esperar para a próxima aventura!'
         },
         {
-            userImage: '/images/Ellipse 2.png',
+            userImage: '../images/Ellipse 2.png',
             userComentario: 'Essa viagem foi uma jornada inesquecível. Desde paisagens deslumbrantes até encontros com pessoas fascinantes, cada momento foi uma nova descoberta.'
         },
         {
-            userImage: '/images/Ellipse 3.png',
+            userImage: '../images/Ellipse 3.png',
             userComentario: 'Viajar me ensinou que o mundo é vasto e cheio de possibilidades emocionantes. Conhecer diferentes modos de vida é uma lição que levarei para sempre. Adorei!'
         },
         {
-            userImage: '/images/Ellipse 4.png',
+            userImage: '../images/Ellipse 4.png',
             userComentario: 'Viajar me ensinou que o mundo é vasto e cheio de possibilidades emocionantes. Conhecer diferentes modos de vida é uma lição que levarei para sempre. Adorei!'
         }
     ];
